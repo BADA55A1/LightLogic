9f8cc2bd6ef60dbb6949d119962aa2b0f05d40fb
Imperatar <imperatar@protonmail.com>
2022-05-28 16:14:35 +0200

    +Output; +TradfriBulb

--- lightlogic.py
+++ lightlogic.py
@@ -6,12 +6,12 @@ import paho.mqtt.client as mqtt
 
 
 class Device:
-	def __init__(self, reference, name = ""):
+	def __init__(self, reference, name = ''):
 		self.name = name
 		self.reference = reference
 	
 
-class Controller(Device):
+class Sensor(Device):
 	def set_action(self, action_callback):
 		self.action_callback = action_callback
 
@@ -25,12 +25,22 @@ class Controller(Device):
 		Device.__init__(self, reference, name)
 		self.action_callback = None
 		self.client = mqtt_client
-		self.client.subscribe("zigbee2mqtt/" + reference)
+		self.client.subscribe('zigbee2mqtt/' + reference)
 		self.client.on_message=self.on_callback
-		
 
 
-class Styrbar(Controller):
+class Output(Device):
+	def __init__(self, reference, name, mqtt_client):
+		Device.__init__(self, reference, name)
+		self.client = mqtt_client
+	
+	def _set(self, property):
+		address = 'zigbee2mqtt/' + self.reference + '/set'
+		self.client.publish(address, json.dumps(property) )
+		print(json.dumps(property))
+
+
+class Styrbar(Sensor):
 	class State(Enum):
 		UNDEFINED = 0
 		UP = 1
@@ -61,23 +71,96 @@ class Styrbar(Controller):
 	}
 
 	def __init__(self, reference, name, mqtt_client):
-		Controller.__init__(self, reference, name, mqtt_client)
-		self.state = self.State.UNDEFINED
+		Sensor.__init__(self, reference, name, mqtt_client)
+		self._state = self.State.UNDEFINED
 	
 	def _process_msg(self, msg_struct):
 		if 'action' in msg_struct:
-			self.state = self.Keys[msg_struct['action']]
+			self._state = self.Keys[msg_struct['action']]
+			self.action_callback(self._state)
 		# print(msg_struct)
-		print(self.name + ": " + str(self.state) )
+		# print(self.name + ": " + str(self._state) )
+
+	def getState(self):
+		return self._state
 
 
+class TradfriBulb(Output):
+	class Temp(Enum):
+		COOLEST = 0
+		COOL = 1
+		NEUTRAL = 2
+		WARM = 3
+		WARMEST = 4
+	
+	TempKeys = {
+		Temp.COOLEST: 'coolest',
+		Temp.COOL:    'cool',
+		Temp.NEUTRAL: 'neutral',
+		Temp.WARM:    'warm',
+		Temp.WARMEST: 'warmest'
+	}
 
+	def __init__(self, reference, name, mqtt_client):
+		Output.__init__(self, reference, name, mqtt_client)
 
-mqtt_host = "192.168.1.111"
-mqtt_port = 1883
-client =mqtt.Client()
-client.connect(mqtt_host, port=mqtt_port)
+	def set(
+		self,
+		power = None,
+		brightness = None,
+		color_hex = None,
+		color_rgb = None,
+		color_temp = None,
+		transition = None
+	):
+		out = {}
 
-s = Styrbar("0x50325ffffed24be5", "Remote A", client)
+		if power is not None:
+			if power == 0: 
+				out['state'] = 'OFF'
+			else:
+				out['state'] = 'ON'
+			
+		if brightness is not None:
+			out['brightness'] = brightness
 
-client.loop_forever()
+		if color_hex is not None:
+			out['color'] = {'hex': color_hex}
+
+		if color_rgb is not None:
+			out['color'] = {'r': color_rgb[0], 'g': color_rgb[1], 'b': color_rgb[2]}
+
+		if color_temp is not None:
+			if isinstance(color_temp, int):
+				out['color_temp'] = color_temp
+			else:
+				out['color_temp'] = self.TempKeys[color_temp]
+			
+		if transition is not None:
+			out['transition'] = transition
+		
+		self._set(out)
+	
+	def move(
+		self,
+		brightness = None,  # int(-254:254), 'stop'
+		color_temp = None   # int(), 'stop'
+	):
+		out = {}
+		if brightness is not None:
+			out['brightness_move'] = brightness
+		if color_temp is not None:
+			out['color_temp_move'] = color_temp
+		self._set(out)
+
+	def step(
+		self,
+		brightness = None,  # int(-254:254), 'stop'
+		color_temp = None   # int(), 'stop'
+	):
+		out = {}
+		if brightness is not None:
+			out['brightness_step'] = brightness
+		if color_temp is not None:
+			out['color_temp_step'] = color_temp
+		self._set(out)
